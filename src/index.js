2ba09ca2c8bd3810439592081eafa0357d3f57ba
PayalPatel20 <patelpayal0222@gmail.com>
2023-09-26 00:57:59 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Ctime from './Ctime';
+import './index.css';
+ReactDOM.render(
+    <Ctime></Ctime>
+    ,document.getElementById('root')
+);
